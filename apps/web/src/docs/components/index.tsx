8e7db29a3ee77ba357f7c2cb6b5ecbdb793e68c8
Arjun Kumar <arjunkmrm@gmail.com>
2026-09-03 16:58:56 +0800

    docs(web): simplify trajectory graphic

--- apps/web/src/docs/components/index.tsx
+++ apps/web/src/docs/components/index.tsx
@@ -11,7 +11,6 @@ import { InterfaceComparisonDiagram } from "./diagrams/InterfaceComparisonDiagra
 import { MethodDiagram } from "./diagrams/MethodDiagram"
 import { PrimitiveDiagram } from "./diagrams/PrimitiveDiagram"
 import { RlmDiagram } from "./diagrams/RlmDiagram"
-import { TrajectoryBranchesDiagram } from "./diagrams/TrajectoryBranchesDiagram"
 import { TransitionLoop } from "./diagrams/TransitionLoop"
 
 const BulbIcon = (): ReactElement => (
@@ -229,7 +228,6 @@ export const mdxComponents = {
   PrimitiveDiagram,
   RlmDiagram,
   Tip,
-  TrajectoryBranchesDiagram,
   TransitionLoop,
   a: (props: ComponentPropsWithoutRef<"a">) => <a {...props} />,
   code: InlineCode,
